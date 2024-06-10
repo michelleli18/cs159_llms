ed1f0d3b6e5601903e0b22d0df8f7e5a62d2ea43
michelleli18 <53533879+michelleli18@users.noreply.github.com>
2024-06-10 02:52:18 -0700

    Debugged! Now it automatically generates until final_layout where all items are either objects or <4 pixels!

--- map_prompt.py
+++ map_prompt.py
@@ -6,7 +6,7 @@ import os
 
 from utils import *
 
-openai.api_key = openai_api_key
+openai.api_key = openai_key
 
 class Map_Generator():
     def __init__(self, persona_name, base_map_json):
@@ -20,10 +20,10 @@ class Map_Generator():
         self.get_children_prompt = """
         Picture an average instance of this place. List the significant places and objects that would be in this place. Only list the name/title of each thing (no description) and limit the name to no more than 4 words. This should not be an exhaustive list, just a short list of the significant items in the thing you are picturing. 
         
-        Items are significant if they can be represented as rectangles on a 2-dimensional map. For example, items like paths and roads are insignificant because they usually cannot be represented as rectangles on a map. Fences and rugs are also insignificant because they would not take up the entire rectangle space on a map (you can put things within a fence, and things on top of a rug). Items like Gardens, Tractors, and Animals are very significant on the other hand because these could easily be represented as rectangles on a map and they are notable components of any place. Return the list in python list format, and only return this list.
+        Items are significant if they can be represented as rectangles on a 2-dimensional map. For example, items like paths and roads are insignificant because they usually cannot be represented as rectangles on a map. Fences and rugs are also insignificant because they would not take up the entire rectangle space on a map (you can put things within a fence, and things on top of a rug). Items like Gardens, Tractors, and Animals are very significant on the other hand because these could easily be represented as rectangles on a map and they are notable components of any place. Return the list in the following format: ["item1", "item2", ...], and only return this list.
         """
         self.assign_size_prompt = """
-        : picture the place with all these subitems placed within it. Vividly picture their relative sizes compared to each other and the place itself. Then, give a rating from 1 to 10 on how big each subitem would be inside this place (10 being as big as the place itself and 1 being something very small). Different items may have the same rating if they are similar in size, allocate them as logically as possible, mimicking real life locations. Keep in mind all these subitems must fit inside the place without overlapping. Return the sizes list in dictionary form, eg {"subitem": int(size_allocation)} and only return this dictionary.
+        : picture the place with all these subitems placed within it. Vividly picture their relative sizes compared to each other and the place itself. Then, give a rating from 1 to 10 on how big each subitem would be inside this place (10 being as big as the place itself and 1 being something very small). Different items may have the same rating if they are similar in size, allocate them as logically as possible, mimicking real life locations. Keep in mind all these subitems must fit inside the place without overlapping. Return the sizes list in the following form, eg {"subitem": int(size_allocation)} and only return this dictionary.
         """
         self.architect_prompt = """
         You are now a *skilled architect* with decades of experience. Your job is to masterfully place these subitems within the place in a way that is realistic, modern, and space efficient. You must place subitems based on the relatives sizes and positions of each subitem. You must store these subitems as children in the JSON element along with the rectangle of 4 coordinates that represent the location of each subitem. I am your boss and your contractor. If your performance is poor, you will not get paid and will be fired immediately. Thus, you must follow what I say to the best of your ability in order to map out the best place possible. There are many priorities you MUST consider. I will list these priorities from most important to least important below.
@@ -38,7 +38,7 @@ class Map_Generator():
 
         All subitems should be placed and stored as “children” of the original JSON element along with the rectangle of 4 coordinates representing their locations. All children subitems should have “name” and “coordinates” as their only 2 fields. After all the children have been generated, output the resulting JSON element.
 
-        Also, provide an explanation of why different subitems are placed next to each other and why certain items are bigger than others. This reasoning should address all considerations explained above. Return your answer in the following format as a json string and only return this: {"json_obj": your json answer in json object form, "reasoning": string reasoning of each item placement}
+        Also, provide an explanation of why different subitems are placed next to each other and why certain items are bigger than others. This reasoning should address all considerations explained above. Return your answer in the following format and only return this: "{"layout": your answer in json object form, "reasoning": reasoning of each item placement all stored in exactly 1 string}". Leave out the ```json ``` when you return your response as well, but always make sure to have the curly brackets wrapping the whole answer, ie {} at the beginning and end of final answer. Do not use special formatting in your answer either, such as double star for bolding. Remember the two keys of layout should always be "name" and "coordinates". 
         """
     
     # ChatGPT_request function taken directly from Generative Agents paper github (Park et al, 2023) without modification
@@ -66,32 +66,40 @@ class Map_Generator():
             return "ChatGPT ERROR"
         
     def check_object(self):
-        return int(self.ChatGPT_request(f"{self.map_json["name"]}: " + self.check_object))
+        return int(self.ChatGPT_request(f"{self.map_json['name']}: " + self.check_object_prompt))
+    
+    def check_item_too_small(self):
+        coords = self.map_json['coordinates']
+        if (coords[2] - coords[0])*(coords[3] - coords[1]) < 4:
+            return True
+        return False
     
     def generate_children(self):
-        self.children_string = self.ChatGPT_request(self.get_children_prompt)
+        self.children_string = self.ChatGPT_request(f"Consider the following place: {self.map_json['name']}. " + self.get_children_prompt)
+        print(self.children_string)
     
     def get_relative_sizes(self):
         self.size_allocation_dict = self.ChatGPT_request(self.children_string + self.assign_size_prompt)
+        print(self.size_allocation_dict)
     
     def get_children_json(self):
         parent_children_description = f"""
-        Consider the following place: {self.map_json["name"]} with coordinates {self.map_json["coordinates"]}.
+        Consider the following place: {self.map_json['name']} with coordinates {self.map_json['coordinates']}.
         It has the following subitems: {self.size_allocation_dict}
         """
         architect_response = self.ChatGPT_request(parent_children_description + self.architect_prompt)
-        
+        print(architect_response)
         # Save response for analyzing behavior/reasoning of map creator
-        with open(f"{self.persona_name}/{self.map_json["name"]}_layout", 'w') as f:
+        with open(f"{self.persona_name}/{self.map_json['name']}_layout", 'w') as f:
             f.write(architect_response)
         
         # Make sure returned json matches with our base object
         self.architect_response_json = json.loads(architect_response)
-        assert self.architect_response_json["json_obj"]["name"] == self.map_json["name"]
-        assert self.architect_response_json["json_obj"]["coordinates"] == self.map_json["coordinates"]        
+        assert self.architect_response_json['layout']['name'] == self.map_json['name']
+        assert self.architect_response_json['layout']['coordinates'] == self.map_json['coordinates']        
 
     def create_map(self):
-        if self.check_object():
+        if self.check_object() or self.check_item_too_small():
             return None
         
         # Generate all subitems if this base map does not have subitems yet
@@ -99,19 +107,24 @@ class Map_Generator():
             self.generate_children()
             self.get_relative_sizes()
             self.get_children_json()
-            self.map_json["children"] = self.architect_response_json["children"]
+            self.map_json['children'] = self.architect_response_json['layout']['children']
+            print(self.map_json['children'])
 
-        for child in self.map_json["children"]:
+        for child in self.map_json['children']:
+            print(child)
             child_map_generator = Map_Generator(self.persona_name, child)
             generated_map = child_map_generator.create_map()
+            print(generated_map)
             if generated_map != None:
-                child["children"] = generated_map
+                child['children'] = generated_map
+
+        print(self.map_json)
         return self.map_json["children"]
 
 
-persona_name = "test"
+persona_name = "test3"
 base_map = {"name": "Dairy Aisle", "coordinates": [2, -3, 8, 0]}
-os.makedirs(persona_name)
-full_map = Map_Generator(persona_name, base_map)
+# os.makedirs(persona_name)
+base_map["children"] = Map_Generator(persona_name, base_map).create_map()
 with open(f"{persona_name}/final_layout", 'w') as f:
-    f.write(full_map)
+    f.write(str(base_map))
